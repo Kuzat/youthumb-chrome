07ac6745005770047e37adcb20b677cff9dde6e6
Vegard Bjørsvik <vegardbjr@gmail.com>
2015-05-16 01:15:32 +0200

    added button.js to web_accessible resources

--- background.js
+++ background.js
@@ -1,16 +1,5 @@
 /* global chrome */
+
 chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
-    if(!(document.getElementById("youthumb"))) {
-		var youtube = document.getElementsByClassName("action-panel-trigger-share")[0];
-		var button = document.createElement("BUTTON"); button.id = "youthumb";
-		var youtube_id = document.querySelectorAll("[itemprop=videoId]")[0].getAttribute("content");
-		button.innerHTML = "<span class=\"yt-uix-button-content\">Thumbnails</span>";
-		button.className = "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity";
-		button.onclick = function() {
-			var win = window.open('https://youthumb.net/watch?v='+youtube_id, "_blank");
-			win.focus();
-		};
-		
-		youtube.parentNode.insertBefore(button, youtube.nextSibling);
-	}
+    chrome.tabs.executeScript(null,{file:"button.js"});
 });
