00b721ce9cecddd19e14673f9bffdd7000986b2d
Vegard Bjørsvik <vegardbjr@gmail.com>
2015-05-16 02:10:24 +0200

    added comments

--- button.js
+++ button.js
@@ -1,24 +1,31 @@
+// function to inssert button
 var insertButton = function() {
 	if(!(document.getElementById("youthumb"))) {
+		// find Share button, create a new button and find the youtube id
 		var youtube = document.getElementsByClassName("action-panel-trigger-share")[0];
-		var button = document.createElement("BUTTON"); button.id = "youthumb";
+		var button = document.createElement("BUTTON");
 		var youtube_id = document.querySelectorAll("[itemprop=videoId]")[0].getAttribute("content");
 		button.innerHTML = "<span class=\"yt-uix-button-content\">Thumbnails</span>";
 		button.className = "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity";
+		// open link to youthumb on click
 		button.onclick = function() {
 			var win = window.open('https://youthumb.net/watch?v='+youtube_id, "_blank");
 			win.focus();
 		};
 		
+		//insert button next to share button
 		youtube.parentNode.insertBefore(button, youtube.nextSibling);
 	};
 };
 
 var isPageLoaded = false;
 
+// new mutation observer to look for changes in the DOM
 var observer = new MutationObserver(function(mutations) {
+	// checks if the youtube body contains page-loaded class
 	var hasPageLoadedClass = mutations[0].target.classList.contains('page-loaded');
 	
+	// if it contains page-loaded class and is not loaded allready
 	if (hasPageLoadedClass && !isPageLoaded) {
 		insertButton();
 	}
@@ -26,6 +33,7 @@ var observer = new MutationObserver(function(mutations) {
 	isPageLoaded = hasPageLoadedClass;
 });
 
+// observe the body for changes in class attribute
 observer.observe(document.querySelector('body'), {
 	attributes: true,
 	attributeFilter: ['class']
